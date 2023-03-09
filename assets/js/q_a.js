cd2932ae831b0af5f70873ebeee2d8e8e82dedd2
VajskiDs <87033268+L10N37@users.noreply.github.com>
2023-03-10 01:24:38 +1100

    Add files via upload

---
+++ assets/js/q_a.js
@@ -0,0 +1,66 @@
+//First 10 Questions from https://www.w3schools.com/quiztest/quiztest.asp?qtest=JS
+//
+
+let questions= {
+    question1:  "Inside which HTML element do we put the JavaScript?",
+    answer1:    "<script>",
+    q1fill1:    "<javascript>",
+    q1fill2:    "<js>",
+    q1fill3:    "<scripting>",
+
+    question2:  "What is the correct JavaScript syntax to change the content of the HTML element below? \n\n<p id=\"demo\"> This is a demonstration.</p>",
+    answer2:    "document.getElementById(\"demo\").innerHTML = \"Hello World!\";",
+    q2fill1:    "document.getElementByName(\"p\").innerHTML = \"Hello World!\";",
+    q2fill2:    "#demo.innerHTML = \"Hello World!\";",
+    q2fill3:    "document.getElement(\"p\").innerHTML = \"Hello World!\";",
+
+    question3:  "Where is the correct place to insert a JavaScript?",
+    answer3:    "Both the <head> section and the <body> section are correct",\
+    q3fill1:    "The <body> section",
+    q3fill2:    "The <head> section",
+    q3fill3:    "The <epic> section",
+
+    question4:  "What is the correct syntax for referring to an external script called \"xxx.js\"?\"",
+    answer4:    "<script src=\"xxx.js\">\"",
+    q4fill1:    "<script href=\"xxx.js\">",
+    q4fill2:    "<script name=\"xxx.js\">",
+    q4fill3:    "<script ref=\"xxx.js\">",
+
+    question5:  "The external JavaScript file must contain the <script> tag.",
+    answer5:    "False",
+    q5fill1:    "True",
+    q5fill2:    "Sometimes",
+    q5fill3:    "Only when required",
+
+    question6:  "How do you write \"Hello World\" in an alert box?\"",
+    answer6:    "alert(\"Hello World\");",
+    q6fill1:    "msgBox(\"Hello World\");",
+    q6fill2:    "alertBox(\"Hello World\");",
+    q6fill3:    "msg(\"Hello World\");",
+
+    question7:  "How do you create a function in JavaScript?",
+    answer7:    "function myFunction()",
+    q7fill1:    "function:myFunction()",
+    q7fill2:    "function.myFunction()",
+    q7fill3:    "function = myFunction()",
+    
+    question8:  "How do you call a function named \"myFunction\"?",
+    answer8:    "myFunction()",
+    q8fill1:    "call function myFunction()",
+    q8fill2:    "call myFunction",
+    q8fill3:    "function.call.myFunction()",
+
+    question9:  "How to write an IF statement in JavaScript?",
+    answer9:    "if (i == 5)",
+    q9fill1:    "if i == 5 then",
+    q9fill2:    "if i = 5 then",
+    q9fill3:    "if i = 5",
+
+    question10: "How to write an IF statement for executing some code if \"i\" is NOT equal to 5?\"",
+    answer10:   "if (i != 5)",
+    q10fill1:   "if i =! 5 then",
+    q10fill2:   "if i <> 5",
+    q10fill3:   "if (i <> 5)"
+  };
+
+
