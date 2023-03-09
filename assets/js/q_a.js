ff9360138c2731e18446efbf3f26147bf8ba3dac
Lionel Sanderson <lfsanderson@hotmail.comgit config --global user.name Lionel>
2023-03-10 07:40:32 +1100

    bug fixes and progress save

--- assets/js/q_a.js
+++ assets/js/q_a.js
@@ -6,17 +6,17 @@ let questions= [
   {
     question:  "Inside which HTML element do we put the JavaScript?",
     answers: ["&ltscript&gt", "&ltjavascript&gt", "&ltjs&gt","&ltscripting&gt"],
-    correctAnswer: ["&ltscript&gt"]
+    correctAnswer: "&ltscript&gt"
   },
   {
     question:  "What is the correct JavaScript syntax to change the content of the HTML element below? \n\n&ltp id=\"demo\"&gt This is a demonstration.&lt/p&gt",
     answers:   ["document.getElementById(\"demo\").innerHTML = \"Hello World!\";", "document.getElementByName(\"p\").innerHTML = \"Hello World!\";", "#demo.innerHTML = \"Hello World!\";", "document.getElement(\"p\").innerHTML = \"Hello World!\";"],
-    correctAnswer: ["document.getElementById(\"demo\").innerHTML = \"Hello World!\";"]
+    correctAnswer: "document.getElementById(\"demo\").innerHTML = \"Hello World!\";"
   },
   {
     question:  "Where is the correct place to insert a JavaScript?",
     answers:   ["Both the &lthead&gt section and the &ltbody&gt section are correct","The &ltbody&gt section","The &lthead&gt section","The &ltepic&gt section"],
-    correctAnswer: ["Both the &lthead&gt section and the &ltbody&gt section are correct"]
+    correctAnswer: "Both the &lthead&gt section and the &ltbody&gt section are correct"
   },
   {
     question:  "What is the correct syntax for referring to an external script called \"xxx.js\"?\"",
@@ -26,30 +26,30 @@ let questions= [
   {
     question:  "The external JavaScript file must contain the &ltscript&gt tag.",
     answers:    ["False","True","Sometimes","Only when required"],
-    correctAnswer: ["False"]
+    correctAnswer: "False"
   },
   {
     question:  "How do you write \"Hello World\" in an alert box?\"",
     answers:    ["alert(\"Hello World\");","msgBox(\"Hello World\");","alertBox(\"Hello World\");","msg(\"Hello World\");"],
-    correctAnswer: ["alert(\"Hello World\");"]
+    correctAnswer: "alert(\"Hello World\");"
   },
   {
     question:  "How do you create a function in JavaScript?",
     answers:    ["function myFunction()","function:myFunction()","function.myFunction()","function = myFunction()"],
-    correctAnswer: ["function myFunction()"]
+    correctAnswer: "function myFunction()"
   },
   {
     question:  "How do you call a function named \"myFunction\"?",
     answers:    ["myFunction()", "call function myFunction()", "call myFunction", "function.call.myFunction()"],
-    correctAnswer: ["myFunction()"]
+    correctAnswer: "myFunction()"
   },
   {
     question:  "How to write an IF statement in JavaScript?",
     answers:    ["if (i == 5)", "if i == 5 then", "if i = 5 then", "if i = 5"],
-    correctAnswer: ["if (i == 5)"]
+    correctAnswer: "if (i == 5)"
   },
   {
     question: "How to write an IF statement for executing some code if \"i\" is NOT equal to 5?\"",
     answers:  ["if (i != 5)","if i =! 5 then","if i &lt&gt 5","if (i &lt&gt 5)"],
-    CorrectAnswer:  ["if (i != 5)"]
+    CorrectAnswer:  "if (i != 5)"
   } ]
--- assets/js/script.js
+++ assets/js/script.js
@@ -1,8 +1,6 @@
-//////////// Globals //////////////
-timeStart = 70;
-index = 0;
-//////////////////////////////////
-
+//////////// Global Area (on load) //////////////
+let timeStart = 70;
+let index = 0;
 // Insert Intro Screen
 let introScreen= document.createElement("div");
 introScreen.className= "intro"
@@ -17,14 +15,22 @@ let introScreenAlt= document.createElement("div");
 introScreenAlt.className= "highLight"
 introScreen.appendChild(introScreenAlt);
 introScreenAlt.innerHTML = "\ You will be penalised 10 seconds for an incorrect answer!";
+////////////////////////////////////////////////
 
 
 function removeIntro(){
     // delete unecessary elements
-    let deleteIntro = document.getElementById('temp');
+    let deleteIntro = document.getElementById('temp', 'imReady');
     deleteIntro.parentNode.removeChild(deleteIntro);
     deleteIntro = document.getElementById('imReady');
     deleteIntro.parentNode.removeChild(deleteIntro);
+}
+
+function clearScreen(){
+    let remove = document.getElementById('createDestroy');
+    remove.parentNode.removeChild(remove);
+    remove = document.getElementById('questionsID');
+    remove.parentNode.removeChild(remove);
 }
 
 function timerDisplay(){
@@ -54,31 +60,6 @@ function startQuiz(){
     document.body.appendChild(questionVar);
     // display question array of objects question # as per index value
     questionVar.innerHTML = questions[index].question;
-    // Generate Answers
-    // parent used for click event listener
-    let answersVarParent= document.createElement("div");
-    answersVarParent.className= "listenTarget";
-    document.body.appendChild(answersVarParent);
-    //1
-    let answersVar0= document.createElement("div");
-    answersVar0.className= "answersClass";
-    answersVar0.id="correctAnswerID";
-    answersVarParent.appendChild(answersVar0);
-    //2
-    let answersVar1= document.createElement("div");
-    answersVar1.className= "answersClass";
-    answersVar1.id="answersID";
-    answersVarParent.appendChild(answersVar1);
-    //3
-    let answersVar2= document.createElement("div");
-    answersVar2.className= "answersClass";
-    answersVar2.id="answersID";
-    answersVarParent.appendChild(answersVar2);
-    //4
-    let answersVar3= document.createElement("div");
-    answersVar3.className= "answersClass";
-    answersVar3.id="answersID";
-    answersVarParent.appendChild(answersVar3);
     // pluck answers from object array, as per current question index value
     // and display in random order each quiz through
     let randomAnswer1to4 =[];
@@ -92,11 +73,45 @@ function startQuiz(){
         AnswersJumbled[i] =  randomAnswer1to4[i];
         console.log(AnswersJumbled[i]);
     }
+    // Generate Answers
+    // parent used for click event listener
+    let answersVarParent= document.createElement("div");
+    answersVarParent.className= "listenTarget";
+    answersVarParent.id="createDestroy"
+    document.body.appendChild(answersVarParent);
+    //1
+    let answersVar0= document.createElement("div");
+    answersVar0.className= "answersClass";
+    answersVar0.id="answersID0";
+    answersVarParent.appendChild(answersVar0);
+    //2
+    let answersVar1= document.createElement("div");
+    answersVar1.className= "answersClass";
+    answersVar1.id="answersID1";
+    answersVarParent.appendChild(answersVar1);
+    //3
+    let answersVar2= document.createElement("div");
+    answersVar2.className= "answersClass";
+    answersVar2.id="answersID2";
+    answersVarParent.appendChild(answersVar2);
+    //4
+    let answersVar3= document.createElement("div");
+    answersVar3.className= "answersClass";
+    answersVar3.id="answersID3";
+    answersVarParent.appendChild(answersVar3);
+    // Inner HTML generated randomly above, this section adjusts the ID of the div containing
+    // the correct answer, to an ID used to ID the correct answer with the click event.
+    if (AnswersJumbled[0] == questions[index].correctAnswer) answersVar0.id="correctAnswerID"
     answersVar0.innerHTML = AnswersJumbled[0];
+    if (AnswersJumbled[1] == questions[index].correctAnswer) answersVar1.id="correctAnswerID"
     answersVar1.innerHTML = AnswersJumbled[1];
+    if (AnswersJumbled[2] == questions[index].correctAnswer) answersVar2.id="correctAnswerID"
     answersVar2.innerHTML = AnswersJumbled[2];
+    if (AnswersJumbled[3] == questions[index].correctAnswer) answersVar3.id="correctAnswerID"
     answersVar3.innerHTML = AnswersJumbled[3];
 
+
+      
         // Add our click event listener for clickable answers
         let clickableAnswers = document.querySelector(".listenTarget");
 
@@ -106,23 +121,16 @@ function startQuiz(){
             if (element.matches("#correctAnswerID")) {
                 alert("fffyeah!");
                 index++;
+                // Clear Screen for next round
+                clearScreen();
                 startQuiz();
                 return;
             }
             // Check if the clicked element was the correct answer
-            if (element.matches("#answersID")) {
+            else {
                 alert("fffno!");
                 timeStart=timeStart-10;
             }
         }
         )
-
-
-
-
-
-
-    
- //  console.log(randomAnswer1to4) //test logger
-       
 }
